c445b1e2679bebe027e606b8cffd19bd680ed45f
Shreeya Timilsina <shreeyatimilsina78@gmail.com>
2026-05-28 18:03:40 +0545

    modified:   config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 SECRET_KEY = "random-secret-key"
-
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "h1elloworld"
-MYSQL_DATABASE = "class_dbip"
+MYSQL_PASSWORD = "shreeya"
+MYSQL_DATABASE = "class_db"
